82f8fd736b06a5e860c50680d9d4c6cf052e56a2
Baytalov <baytalov91@gmail.com>
2023-03-11 21:28:55 +0500

    Update js-css.php

--- configure/js-css.php
+++ configure/js-css.php
@@ -1,7 +1,7 @@
 <?php
 
 function _add_javascript() {
-    // force all scripts to load in footer
+    // заставить все скрипты загружаться в footer
     remove_action('wp_head', 'wp_print_scripts');
     remove_action('wp_head', 'wp_print_head_scripts', 9);
     remove_action('wp_head', 'wp_enqueue_scripts', 1);
@@ -11,7 +11,7 @@ function _add_javascript() {
 add_action('wp_enqueue_scripts', '_add_javascript', 100);
 
 function _add_stylesheets() {
-    // removing all WP css files enqueued by default
+    // удаление всех файлов css WP, поставленных в очередь по умолчанию
     wp_dequeue_style('wp-block-library');
     wp_dequeue_style('wp-block-library-theme');
     wp_dequeue_style('wc-block-style');

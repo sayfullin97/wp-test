b02b38f71685d52fcd179ab6c87eb1334cc6f5b1
Baytalov <baytalov91@gmail.com>
2023-03-11 21:26:58 +0500

    Update configure.php

--- configure/configure.php
+++ configure/configure.php
@@ -1,6 +1,6 @@
 <?php
 
-// MENUS
+// МЕНЮ
 function _custom_theme_register_menu() {
     register_nav_menus(
         array(
@@ -21,32 +21,32 @@ function custom_setup() {
     // Languages
     load_theme_textdomain('textdomaintomodify', get_template_directory() . '/languages');
 
-    // HTML 5 - Example : deletes type="*" in scripts and style tags
+    // HTML 5 — пример: удаляет type="*" в скриптах и тегах стилей
     add_theme_support( 'html5', [ 'script', 'style' ] );
 
-    // Remove SVG and global styles
+    // Удалить SVG и глобальные стили
     remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
     remove_action('wp_body_open', 'wp_global_styles_render_svg_filters' );
 
-    // Remove wp_footer actions which add's global inline styles
+    // Удалите действия wp_footer, которые добавляют глобальные встроенные стили.
     remove_action('wp_footer', 'wp_enqueue_global_styles', 1);
 
-    // Remove render_block filters which adds unnecessary stuff
+    // Удалите фильтры render_block, которые добавляют ненужные вещи.
     remove_filter('render_block', 'wp_render_duotone_support');
     remove_filter('render_block', 'wp_restore_group_inner_container');
     remove_filter('render_block', 'wp_render_layout_support_flag');
 
-	// Remove useless WP image sizes
+	// Удаляем бесполезные размеры изображений WP
 	remove_image_size( '1536x1536' );
 	remove_image_size( '2048x2048' );
 
-	// Custom image sizes
+	// Пользовательские размеры изображений
 	// add_image_size( '424x424', 424, 424, true );
 	// add_image_size( '1920', 1920, 9999 );
 }
 add_action('after_setup_theme', 'custom_setup');
 
-// remove default image sizes to avoid overcharging server - comment line if you need size
+// удалите размеры изображений по умолчанию, чтобы избежать перегрузки сервера - строка комментария, если вам нужен размер
 function remove_default_image_sizes( $sizes) {
 	unset( $sizes['large']);
 	unset( $sizes['medium']);
@@ -55,34 +55,34 @@ function remove_default_image_sizes( $sizes) {
 }
 add_filter('intermediate_image_sizes_advanced', 'remove_default_image_sizes');
 
-// disabling big image sizes scaled
+// отключение масштабирования больших изображений
 add_filter( 'big_image_size_threshold', '__return_false' );
 
-// Giving credits
+// Предоставление кредитов
 function remove_footer_admin () {
-	echo 'Thème crée par <a href="http://www.olivier-guilleux.com" target="_blank">Olivier Guilleux</a>';
+	echo 'Сайт разработал <a href="#!" target="_blank">Baytalov Karen</a>';
 }
 add_filter('admin_footer_text', 'remove_footer_admin');
 
-// Move Yoast to bottom
+// Переместить Yoast вниз
 function yoasttobottom() {
 	return 'low';
 }
 add_filter( 'wpseo_metabox_prio', 'yoasttobottom');
 
-// Remove WP Emoji
+// Удалить эмодзи WP
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
-// delete wp-embed.js from footer
+// удалить wp-embed.js из нижнего колонтитула
 function my_deregister_scripts() {
 	wp_deregister_script( 'wp-embed' );
 }
 add_action( 'wp_footer', 'my_deregister_scripts' );
 
-// delete jquery migrate
+// удаляем миграцию jquery
 function dequeue_jquery_migrate( &$scripts){
 	if(!is_admin()){
 		$scripts->remove( 'jquery');
@@ -91,7 +91,7 @@ function dequeue_jquery_migrate( &$scripts){
 }
 add_filter( 'wp_default_scripts', 'dequeue_jquery_migrate' );
 
-// add SVG to allowed file uploads
+// добавить SVG к разрешенным загрузкам файлов
 function add_file_types_to_uploads($mime_types) {
 	$mime_types['svg'] = 'image/svg+xml';
 
@@ -99,6 +99,6 @@ function add_file_types_to_uploads($mime_types) {
 }
 add_action('upload_mimes', 'add_file_types_to_uploads', 1, 1);
 
-//disable update emails
+//отключаем обновления по электронной почте
 add_filter( 'auto_plugin_update_send_email', '__return_false' );
 add_filter( 'auto_theme_update_send_email', '__return_false' );
